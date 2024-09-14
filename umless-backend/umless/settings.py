e686da9bb5e116be0f4f50f78c20a9390a5cc4bd
kkathereliu <katherineliu327kl@gmail.com>
2024-09-14 11:38:17 -0400

    dockerized django service

--- umless-backend/umless/settings.py
+++ umless-backend/umless/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$wf%c3gnwzb*^pdkq6skz777ymidvt5urr&egau2%#^@)h!$bd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
